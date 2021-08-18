2da9949423e800b64b36adbdb816365668a974a2
Kaia Parks <kaia@kaiaparks.me>
2021-08-17 20:24:03 -0700

    style: change to use JSCS

--- glassed@kaiaparks/extension.js
+++ glassed@kaiaparks/extension.js
@@ -7,42 +7,45 @@ var GLib = imports.gi.GLib;
 
 let _settings = null;
 
-function set_opacity(windows_surfaces) {
-	let opacity_value = _settings.get_int('window-opacity');
-	windows_surfaces.forEach(surface => {
-		surface.opacity = opacity_value;
-	});
+function setOpacity(windowSurfaces) {
+  let opacityValue = _settings.get_int('window-opacity');
+  windowSurfaces.forEach(surface => {
+    surface.opacity = opacityValue;
+  });
 }
 
-function get_window_surfaces(meta_window) {
-	let window_actor = meta_window.get_compositor_private();
-	let childs = window_actor.get_children();
-	let surfaces = childs.filter(child => child.constructor.name.indexOf('MetaSurfaceActor') > -1);
-	if (surfaces.length > 0) {
-		return surfaces;
-	}
+function getWindowSurfaces(metaWindow) {
+  let windowActor = metaWindow.get_compositor_private();
+  let childs = windowActor.get_children();
+  let surfaces = childs.filter(child =>
+    child.constructor.name.indexOf('MetaSurfaceActor') > -1);
+  if (surfaces.length > 0) {
+    return surfaces;
+  }
 
-	return [window_actor];
+  return [windowActor];
 }
 
-function window_created(meta_display, meta_window){
-	let window_surfaces = get_window_surfaces(meta_window);
-	GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, set_opacity(window_surfaces));
+function onWindowCreated(metaDisplay, metaWindow) {
+  let windowSurfaces = getWindowSurfaces(metaWindow);
+  GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, setOpacity(windowSurfaces));
 }
 
 function enable() {
-	_settings = ExtensionUtils.getSettings();
-	_on_window_created = global.display.connect('window-created', window_created);
-	//_on_window_focused = global.display.connect('window-focus', window_created);
+  _settings = ExtensionUtils.getSettings();
+  _onWindowCreated = global.display.connect('window-created', onWindowCreated);
+
+  //_on_window_focused = global.display.connect('window-focus', window_created);
 }
 
 function disable() {
-	global.display.disconnect(_on_window_created);
-	//global.display.disconnect(_on_window_focused);
+  global.display.disconnect(_onWindowCreated);
 
-	_settings.run_dispose();
+  //global.display.disconnect(_on_window_focused);
+
+  _settings.run_dispose();
 }
 
 function init() {
-	ExtensionUtils.initTranslations();
+  ExtensionUtils.initTranslations();
 }
